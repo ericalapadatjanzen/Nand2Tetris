c6b0f8f5273165ee8b68b004f12313fdd5a8d536
ericalapadatjanzen@gmail.com <ericalapadatjanzen@gmail.com>
2017-08-15 21:00:23 -0700

    finished mult, working on fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,124 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+
+
+(START)
+
+
+//Initalize values//
+
+    @SCREEN
+    D=A  // D = 16384
+    @pointer
+    M=D  // RAM[16] = 16384
+
+//keyboard memory map location RAM[24576]
+
+
+    @KBD
+    D=M // D = keyboard (R24576) is value of 0 unless 1 if key pressed
+
+    @WHITE
+    D;JEQ // no key pressed, screen to go white
+
+    @BLACK
+    D;JGT //key pressed, screen to go black
+
+(BLACK)
+
+    @colour
+    M=0  //set colour value to 0
+
+    @FILL
+    0;JMP //goto FILL
+
+(WHITE)
+
+    @colour
+    M=-1  //set colour value to -1
+
+    @FILL
+    0;JMP //goto FILL
+
+(FILL)
+
+
+    @pointer
+    D=M  //D = RAM[16384]
+
+    @KBD
+    D=D - M // 16384 - 0 or 1
+
+    @START
+    D;JEQ  // goto start if D = 0  - key not pressed
+
+    @colour
+    D = M  //D = 0 or - 1
+
+    @pointer
+    A = M  //
+    M = D  //
+
+    @pointer
+    M = M + 1
+
+    @FILL
+    0;JMP //goto fill
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // @16384
+   // D = M  // D = R16384
+
+    // D = D + 1 // D = R16385
+
+
+
+   // @24757
+   // D=M // D = 24757
+
+   // @addr
+   // D = D + A  // 24757 + 0 or -1
+
+   // @16384
+   // D =
+
+
+
+
+
+
+
+
+
+
+
+    //@row
+    //M=255
+    //8160
+
+    //@colum
+    //M=511
+    //32
+
+     // screen start RAM[16384]
+
+     // screen end RAM[24757]
+     // 8192 words of memory each screen word controls 16 pixels
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,51 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+
+// set index to 1
+
+    @i
+    M=1
+
+//set R2 to 0
+
+    @2
+    M=0
+
+(LOOP)
+
+    @0
+    D=M   // D=R0\
+    @END
+    D;JLE  // if D = 0 goto END
+
+
+    @1
+    D=M  // D=R1
+    @END
+    D;JLE  // if D = 0 goto END
+
+    @i
+    D=M  // D=i
+
+    @1
+    D=D-M  // D = i - R1
+    @END
+    D;JGT  // if (i - r1) > 0 goto END
+
+
+    @0
+    D=M  // D = R0
+
+    @2
+    M=D+M  // R2 = R0 + R2
+
+    @i
+    M=M+1  // i = i + 1
+    @LOOP
+    0;JMP  // Keep LOOOOOOOOPING
+
+(END)
+    @END
+    0;JMP
